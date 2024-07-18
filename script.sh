5f3d26909970b5fce2c7b3f8dd85b75f0db96cc1
Stakeme <63229943+ProNodes11@users.noreply.github.com>
2024-07-18 13:52:53 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo rm -rf AethirCheckerCLI-linux-1.0.2.3-eu.tar.gz AethirCheckerCLI-linux-eu
+sudo rm -rf AethirCheckerCLI-linux-1.0.2.5-eu.tar.gz AethirCheckerCLI-linux-eu-1.0.2.5
 
 wget https://checker-mainet-s3.s3.ap-southeast-1.amazonaws.com/eu/AethirCheckerCLI-linux-1.0.2.6.tar.gz
 
