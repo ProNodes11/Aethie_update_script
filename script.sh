e3ecb78c1df97d1cafd19c998969e557da14026f
Stakeme <63229943+ProNodes11@users.noreply.github.com>
2024-07-18 13:46:00 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo rm -rf AethirCheckerCLI-linux-1.0.2.0-eu.tar.gz AethirCheckerCLI-linux-eu
+sudo rm -rf AethirCheckerCLI-linux-1.0.2.3-eu.tar.gz AethirCheckerCLI-linux-eu
 
-wget https://checker-mainet-s3.s3.ap-southeast-1.amazonaws.com/eu/AethirCheckerCLI-linux-1.0.2.3.tar.gz
+wget https://checker-mainet-s3.s3.ap-southeast-1.amazonaws.com/eu/AethirCheckerCLI-linux-1.0.2.5.tar.gz
 
-tar -xvf AethirCheckerCLI-linux-1.0.2.3.tar.gz
+tar -xvf AethirCheckerCLI-linux-1.0.2.5.tar.gz
 
-cd AethirCheckerCLI-linux-1.0.2.3/
+cd AethirCheckerCLI-linux-1.0.2.5/
 
 sudo ./install.sh
